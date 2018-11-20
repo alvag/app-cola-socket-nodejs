d8cd4a92891e985f7dd1dd9b88faa99a39fc0fde
Max Alva <alva85@gmail.com>
2018-11-20 14:52:51 -0500

    Siguiente ticket

---
+++ server/classes/ticket-control.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+class TicketControl {
+
+    constructor() {
+        this.last = 0;
+        this.today = new Date().getDate();
+
+        let jsonData = require('../data/data.json');
+
+        if (jsonData.today === this.today) {
+            this.last = jsonData.last;
+        } else {
+            this.resetCount();
+        }
+    }
+
+    nextTicket() {
+        this.last++;
+        this.writeJsonFile();
+
+        return `Ticket ${this.last}`;
+    }
+
+    resetCount() {
+        this.last = 0;
+        this.writeJsonFile();
+    }
+
+    writeJsonFile() {
+        let jsonDataString = JSON.stringify({
+            last: 0,
+            today: this.today
+        });
+        fs.writeFileSync('./server/data/data.json', jsonDataString);
+    }
+
+}
+
+module.exports = {
+    TicketControl
+};
