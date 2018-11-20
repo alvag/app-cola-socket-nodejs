822e96fbecc29eb1f04135d4350c493a38346e19
Max Alva <alva85@gmail.com>
2018-11-20 16:23:08 -0500

    Atender un ticket

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -4,15 +4,18 @@ const { Ticket } = require('./ticket');
 class TicketControl {
 
     constructor() {
+        this.limitLast = 4;
         this.last = 0;
         this.today = new Date().getDate();
         this.tickets = [];
+        this.lastTickets = [];
 
         let jsonData = require('../data/data.json');
 
         if (jsonData.today === this.today) {
             this.last = jsonData.last;
             this.tickets = jsonData.tickets;
+            this.lastTickets = jsonData.lastTickets;
         } else {
             this.resetCount();
         }
@@ -33,9 +36,31 @@ class TicketControl {
         return `Ticket ${this.last}`;
     }
 
+    attendTicket(desktop) {
+        if (this.tickets.length === 0) {
+            return 'No hay tickets';
+        }
+
+        let numberTicket = this.tickets[0].number;
+        this.tickets.shift();
+
+        let attendTicket = new Ticket(numberTicket, desktop);
+
+        this.lastTickets.unshift(attendTicket);
+
+        if (this.lastTickets > this.limitLast) {
+            this.lastTickets.pop();
+        }
+
+        this.writeJsonFile();
+
+        return attendTicket;
+    }
+
     resetCount() {
         this.last = 0;
         this.tickets = [];
+        this.lastTickets = [];
         this.writeJsonFile();
     }
 
@@ -43,7 +68,8 @@ class TicketControl {
         let jsonDataString = JSON.stringify({
             last: this.last,
             today: this.today,
-            tickets: this.tickets
+            tickets: this.tickets,
+            lastTickets: this.lastTickets
         });
         fs.writeFileSync('./server/data/data.json', jsonDataString);
     }
