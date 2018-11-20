60e01bbac280f24a1313490fc70e1e3aa903c8ca
Max Alva <alva85@gmail.com>
2018-11-20 16:05:11 -0500

    coleccion de tickets pendientes de atender

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
+const { Ticket } = require('./ticket');
 
 class TicketControl {
 
     constructor() {
         this.last = 0;
         this.today = new Date().getDate();
+        this.tickets = [];
 
         let jsonData = require('../data/data.json');
 
         if (jsonData.today === this.today) {
             this.last = jsonData.last;
+            this.tickets = jsonData.tickets;
         } else {
             this.resetCount();
         }
@@ -17,6 +20,10 @@ class TicketControl {
 
     nextTicket() {
         this.last++;
+
+        let ticket = new Ticket(this.last, null);
+        this.tickets.push(ticket);
+
         this.writeJsonFile();
 
         return `Ticket ${this.last}`;
@@ -28,13 +35,15 @@ class TicketControl {
 
     resetCount() {
         this.last = 0;
+        this.tickets = [];
         this.writeJsonFile();
     }
 
     writeJsonFile() {
         let jsonDataString = JSON.stringify({
             last: this.last,
-            today: this.today
+            today: this.today,
+            tickets: this.tickets
         });
         fs.writeFileSync('./server/data/data.json', jsonDataString);
     }
